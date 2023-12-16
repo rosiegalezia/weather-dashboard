d09b83eb32ad1aa14480a13a08baef4499014acb
rosiegalezia <rosegalezia@gmail.com>
2023-12-16 14:49:10 +0000

    Initial commit

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+// API key: a5fc6a3bb1ef51f3168ed91a99397fb3
+
+
+// Access user's search input
+// Get the coordinates for the location
+// Variables to link to these coordinates in the URL
+
+// queryURL: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}.
+
+
+// When a user searches for a city they are presented with current and future conditions for that city
+// that city is added to the search history
+
+// When a user views the current weather conditions for that city they are presented with:
+// The city name
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+// The wind speed
+
+// When a user view future weather conditions get a 5-day forecast that displays:
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+
+// When a user clicks on a city in the search history
+// they are again presented with current and future conditions for that city
