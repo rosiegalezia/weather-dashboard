30e4076cd11e16f42bf0eda33002cc275071f903
rosiegalezia <rosegalezia@gmail.com>
2023-12-18 14:13:52 +0000

    added reset.css and got the weather icons to display for current weather

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,5 @@
 // API key: a5fc6a3bb1ef51f3168ed91a99397fb3
 
-// Access user's search input
-// const search = $(".weather-search").val()
-
-
 const search = $("#search-form") // variable for form input
 const searchInput = $(".weather-search") // variable to access search term
 
@@ -22,36 +18,35 @@ search.on("submit", function (event) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.val() + ",uk&appid=a5fc6a3bb1ef51f3168ed91a99397fb3" + "&units=metric"
     console.log(queryURL)
 
-    // Call the API and render the result in the HTML
-    //        - Get the city name and show it in the main weather forecast card
-    //        - Get the first weather forecast item and get the following values
-    //            - date
-    //            - temperature
-    //            - wind speed
-    //            - humidity
-    //            - icon
-
     fetch(queryURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(queryURL)
+            // clear the currentweather text content from previous search
+            $(".current-weather").text("")
 
+            console.log(queryURL)
             console.log(data)
+
+            var iconcode = data.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
             var date = $("<p>").text(dayjs().format("dddd, D/M/YYYY, HH:mm"))
             var temp = $("<p>").text("Temperature: " + data.main.temp + " C")
             var wind = $("<p>").text("Wind speed: " + data.wind.speed + " km/h")
             var humidity = $("<p>").text("Humidity: " + data.main.humidity + " %")
-            // icon
+            var icon = $("<img>").attr('src', iconurl);
 
             $(".current-weather").append(date) 
             $(".current-weather").append(temp)
             $(".current-weather").append(wind)
             $(".current-weather").append(humidity)
+            $(".current-weather").append(icon)
         });
 
+            // 5-DAY FORECAST REQUIRES A DIFFERENT API
+            // https://openweathermap.org/forecast5
 
 
 })
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-dashboard
 A weather dashboard created for the week 8 challenge to allow travellers to see the weather forecast in multiple cities
+
+https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon 
